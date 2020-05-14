e7dc3afb44e76e8531ecc67181c097bf6d6552f3
Alvaro Castillo Correa <alvarocastillocorrea@alvarocastle.local>
2020-05-14 03:18:49 -0500

    Deleting client

--- client/src/pages/NotFoundPage.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const NotFoundPage = () => {
-  return (
-    <>
-      <h1>404 Page Not Found</h1>
-    </>
-  );
-};
--- client/src/styles/style.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const PageBody = styled.div`
-  max-width: 700px;
-  margin: auto;
-  padding-top: 64px;
-  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
-  font-size: 18px;
-`;
